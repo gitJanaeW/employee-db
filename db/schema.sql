60d49f004772a5d002a6e0312f4f8f41e8d1bc36
janae.wel <janae.wel@gmail.com>
2022-08-13 13:36:14 -0400

    schema done

--- db/schema.sql
+++ db/schema.sql
@@ -15,10 +15,16 @@ CREATE TABLE role (
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
--- CREATE TABLE employee (
---     id INTEGER AUTO_INCREMENT PRIMARY KEY,
---     first_name VARCHAR(30) NOT NULL,
---     last_name VARCHAR(30) NOT NULL,
---     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
---     CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) NOT NULL
--- );
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER NOT NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+);
+
+DESCRIBE department;
+DESCRIBE role;
+DESCRIBE employee;
