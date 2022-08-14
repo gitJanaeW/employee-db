25fc1c15030879ae0e22a28b368fda7f1b35c1e3
janae.wel <janae.wel@gmail.com>
2022-08-13 23:08:10 -0400

    fixing select statement

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const questions = () => {
         {
             type: 'list',
             name: 'action',
-            message: `\n- = = = = = = = = = = = = = = = = = -\n| E M P L O Y E E  D A T A B A S E |\n- = = = = = = = = = = = = = = = = = -\nWhat would you like to do?`,
+            message: `\n- = = = = = = = = = = = = = = = = = -\n| E M P L O Y E E   D A T A B A S E |\n- = = = = = = = = = = = = = = = = = -\nWhat would you like to do?`,
             choices: ['View Employees', 'View Roles', 'View Departments']
         }
     ).then(({action}) => viewDatabase(action));
@@ -16,11 +16,17 @@ const viewDatabase = (section) => {
     if (section === 'View Employees') {
         const sql = `SELECT * FROM employee;`
         db.query(sql, (err, rows) => {
-            console.table(rows);
+            const employeeArr = rows;
+            const sansId =  employeeArr.forEach(() => {
+                delete employeeArr.department_id;
+            });
+            console.table(sansId);
         });
-    }
-    else if (section === 'View Roles') {
-        const sql = `Select * FROM role;`
+    } else if (section === 'View Roles') {
+        const sql = `Select role.*, department.name AS department_name 
+            FROM role 
+            LEFT JOIN department ON
+            role.department_id = department.id;`
         db.query(sql, (err, rows) => {
             console.table(rows);
         });
