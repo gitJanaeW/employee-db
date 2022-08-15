fea0f4e4f1f8672de843d93cd03a5bad64aeede8
janae.wel <janae.wel@gmail.com>
2022-08-14 21:04:27 -0400

    adding Add Employee section

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const questions = () => {
             type: 'list',
             name: 'action',
             message: `\n- = = = = = = = = = = = = = = = = = -\n| E M P L O Y E E   D A T A B A S E |\n- = = = = = = = = = = = = = = = = = -\nWhat would you like to do?`,
-            choices: ['View Employees', 'View Roles', 'View Departments']
+            choices: ['View Employees', 'View Roles', 'View Departments', 'Add Employee', 'Update Employee Role', 'Add Role', 'Add Department']
         }
-    ).then(({action}) => viewDatabase(action));
+    )
 }
 
 const viewDatabase = (section) => {
@@ -22,7 +22,6 @@ const viewDatabase = (section) => {
         employee.role_id = role.id
         LEFT JOIN department ON
         role.department_id = department.id;`
-        
         db.query(sql, (err, rows) => {
             console.table(rows);
         });
@@ -34,12 +33,53 @@ const viewDatabase = (section) => {
         db.query(sql, (err, rows) => {
             console.table(rows);
         });
-    } else {
+    } else if (section === 'View Departments') {
         const sql = `SELECT * FROM department;`
         db.query(sql, (err, rows) => {
             console.table(rows);
         });
+    } else if (section === 'Add Employee') {
+        inquireEmployee();
     }
 }
 
-questions();
+const inquireEmployee = () => {
+    console.log('in inquirerEmp')
+    inquirer.prompt(
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'What is the first name of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'What is the last name of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'role',
+            message: 'What is the job title of the employee?'
+        },
+        {
+            type: 'confirm',
+            name: 'firstName',
+            message: 'Is the employee a manager?',
+            default: false
+        }
+    ).then((employeeInfo) => addEmployee(employeeInfo));
+    
+}
+
+const addEmployee = (employee) => {
+    console.log(employee);
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager) VALUES
+    (${firstName}, ${lastName}, ${roleId}, ${manager})`;
+    db.query(sql, (err, row) => {
+        console.log('Employee added.');
+        console.table(row);
+    });
+}
+
+questions()
+    .then(({action}) => viewDatabase(action));
