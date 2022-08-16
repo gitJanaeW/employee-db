13c433c1dbe646f135e7fa18a5a06890d837a969
janae.wel <janae.wel@gmail.com>
2022-08-15 21:31:52 -0400

    add employee functional

--- index.js
+++ index.js
@@ -7,14 +7,39 @@ const questions = () => {
             type: 'list',
             name: 'action',
             message: `\n- = = = = = = = = = = = = = = = = = -\n| E M P L O Y E E   D A T A B A S E |\n- = = = = = = = = = = = = = = = = = -\nWhat would you like to do?`,
-            choices: ['View Employees', 'View Roles', 'View Departments', 'Add Employee', 'Update Employee Role', 'Add Role', 'Add Department']
+            choices: [
+                'View Employees',
+                'View Roles',
+                'View Departments',
+                'Add Employee',
+                'Update Employee Role',
+                'Add Role',
+                'Add Department',
+                'EXIT'
+            ]
         }
     )
 }
 
 const viewDatabase = (section) => {
-    if (section === 'View Employees') {
-        const sql = `SELECT employee.first_name, employee.last_name,
+    switch (section) {
+        case 'View Employees':
+            viewEmployees();
+            break;
+        case 'View Roles':
+            viewRoles();
+            break;
+        case 'View Departments':
+        viewDepartments();
+        break;
+        case 'Add Employee':
+            inquireEmployee();
+            break;
+    }
+}
+
+const viewEmployees = () => {
+    const sql = `SELECT employee.first_name, employee.last_name,
         role.title AS title, role.salary AS salary,
         department.name AS department, employee.manager
         FROM employee
@@ -22,25 +47,24 @@ const viewDatabase = (section) => {
         employee.role_id = role.id
         LEFT JOIN department ON
         role.department_id = department.id;`
-        db.query(sql, (err, rows) => {
-            console.table(rows);
-        });
-    } else if (section === 'View Roles') {
-        const sql = `Select role.title, department.name AS department, role.salary
+    db.query(sql, (err, rows) => {
+        console.table(rows);
+    });
+}
+const viewRoles = () => {
+    const sql = `Select role.title, department.name AS department, role.salary
         FROM role 
         LEFT JOIN department ON
         role.department_id = department.id;`
-        db.query(sql, (err, rows) => {
-            console.table(rows);
-        });
-    } else if (section === 'View Departments') {
-        const sql = `SELECT * FROM department;`
-        db.query(sql, (err, rows) => {
-            console.table(rows);
-        });
-    } else if (section === 'Add Employee') {
-        inquireEmployee();
-    }
+    db.query(sql, (err, rows) => {
+        console.table(rows);
+    });
+}
+const viewDepartments = () => {
+    const sql = `SELECT * FROM department;`
+    db.query(sql, (err, rows) => {
+        console.table(rows);
+    });
 }
 
 const inquireEmployee = () => {
@@ -58,23 +82,36 @@ const inquireEmployee = () => {
         {
             type: 'input',
             name: 'role',
-            message: 'What is the job title of the employee?'
+            message: 'What is the role id of the employee?'
         },
         {
             type: 'confirm',
-            name: 'firstName',
+            name: 'manager',
             message: 'Is the employee a manager?',
             default: false
         }
-    ])
-    .then(employeeInfo => addEmployee(employeeInfo));
-    
+    ]).then(employeeInfo => inquirerManager(employeeInfo));   
 }
 
-const addEmployee = (employee) => {
-    console.log("LOOOK HERE");
+const inquirerManager = (employee) => {
+    if (employee.manager) {
+        inquirer.prompt({
+            type: 'input',
+            name: 'managerId',
+            message: 'What is the id number of their manager?'
+        }).then(({managerId}) => addEmployee(parseInt(managerId), employee));
+        return;
+    }
+    addEmployee('NULL', employee);
+}
+
+const addEmployee = (managerId, employee) => {
+    delete employee.manager;
+    employee.managerId = managerId;
+    employee.role = parseInt(employee.role);
+    console.log(employee);
     const sql = `INSERT INTO employee (first_name, last_name, role_id, manager) VALUES
-    (${firstName}, ${lastName}, ${roleId}, ${manager})`;
+    ('${employee.firstName}', '${employee.lastName}', '${employee.role}', '${employee.managerId}')`;
     db.query(sql, (err, row) => {
         console.log('Employee added.');
         console.table(row);
@@ -83,3 +120,4 @@ const addEmployee = (employee) => {
 
 questions()
     .then(({action}) => viewDatabase(action));
+    // .then(questions);
